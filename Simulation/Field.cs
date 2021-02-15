d19fa4f55e931276d27a7dd897a7cebe26cd8721
Mikk Leini <mikk.leini@gmail.com>
2021-02-15 21:25:07 +0200

    Better code structure.

---
+++ Simulation/Field.cs
@@ -0,0 +1,182 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Numerics;
+using System.Text;
+using System.Threading.Tasks;
+using Box2D.NetStandard;
+using Box2D.NetStandard.Collision;
+using Box2D.NetStandard.Collision.Shapes;
+using Box2D.NetStandard.Common;
+using Box2D.NetStandard.Dynamics;
+using Box2D.NetStandard.Dynamics.Bodies;
+using Box2D.NetStandard.Dynamics.Fixtures;
+using Box2D.NetStandard.Dynamics.World;
+
+namespace Simulation
+{
+    /// <summary>
+    /// Simulated field
+    /// </summary>
+    public class Field
+    {
+        private World world;
+
+        /// <summary>
+        /// Constructor
+        /// </summary>
+        public Field()
+        {
+            world = new World(new Vector2(0, 0));
+        }
+
+        /// <summary>
+        /// Ray-tracing standard deviation error in meters.
+        /// </summary>
+        public float RayTraceError { get; set; } = 0.01f;
+
+        /// <summary>
+        /// Create default field
+        /// </summary>
+        public void CreateDefaultField()
+        {
+            Vector2[] outerEdges = new Vector2[]
+            {
+                new Vector2(-15.0f, -10.0f),
+                new Vector2(+15.0f, -10.0f),
+                new Vector2(+15.0f,  -7.0f),
+                new Vector2(+12.0f,  -6.0f),
+                new Vector2(+10.0f,  -2.0f),
+                new Vector2(+15.0f,  -2.0f),
+                new Vector2(+15.0f, +10.0f),
+                new Vector2(-7.0f,  +10.0f),
+                new Vector2(-7.0f,  +5.0f),
+                new Vector2(-8.0f,  +5.0f),
+                new Vector2(-8.0f,  +10.0f),
+                new Vector2(-15.0f, +10.0f),
+            };
+
+            AddEdges(outerEdges, true);
+
+            Vector2[] innerEdges = new Vector2[]
+            {
+                new Vector2(-10.0f, -5.0f),
+                new Vector2(-8.0f,  -5.0f),
+                new Vector2(-6.0f,  +1.0f),
+                new Vector2(-8.0f, +2.0f),
+            };
+
+            AddEdges(innerEdges, true);
+        }
+
+        /// <summary>
+        /// Add edges to the field
+        /// </summary>
+        /// <param name="vertices"></param>
+        /// <param name="closeLoop">Close loop ?</param>
+        public void AddEdges(Vector2[] vertices, bool closeLoop)
+        {
+            var bodyDef = new BodyDef
+            {
+                type = BodyType.Static,
+                position = new Vector2(0, 0)
+            };
+
+            var body = world.CreateBody(bodyDef);
+
+            // Create edge shapes. They support two-side collision
+            Vector2 prev = vertices.First();
+            foreach (Vector2 next in vertices.Skip(1))
+            {
+                var edgeShape = new EdgeShape(prev, next);
+                var fixtureDef = new FixtureDef
+                {
+                    shape = edgeShape
+                };
+
+                body.CreateFixture(fixtureDef);
+                prev = next;
+            }
+
+            // Close loop ?
+            if (closeLoop)
+            {
+                Vector2 next = vertices.First();
+
+                var edgeShape = new EdgeShape(prev, next);
+                var fixtureDef = new FixtureDef
+                {
+                    shape = edgeShape
+                };
+
+                body.CreateFixture(fixtureDef);
+            }
+        }
+
+        /// <summary>
+        /// Get all edges as lines (two points)
+        /// </summary>
+        /// <returns></returns>
+        public IEnumerable<(Vector2, Vector2)> GetEdges()
+        {
+            // Iterate over all bodies
+            Body body = world.GetBodyList();
+            while (body != null)
+            {
+                // Iterate over all fixtures
+                Fixture fixture = body.GetFixtureList();
+                while (fixture != null)
+                {
+                    switch (fixture.Shape)
+                    {
+                        case EdgeShape edgeShape:
+                            yield return new(edgeShape.Vertex1, edgeShape.Vertex2);
+                            break;
+
+                        case ChainShape chainShape:
+                            //RenderPolygon(chainShape.Vertices, Brushes.Black, 0.1);
+                            break;
+
+                        case PolygonShape polyShape:
+                            //RenderPolygon(polyShape.GetVertices(), Brushes.Black, 0.1);
+                            break;
+                    }
+
+                    fixture = fixture.GetNext();
+                }
+
+                body = body.GetNext();
+            }
+        }
+
+        /// <summary>
+        /// Ray-trace and get closest hit
+        /// </summary>
+        /// <param name="pos">Ray emitting position</param>
+        /// <param name="angle">Ray emitting angle</param>
+        /// <param name="maxDist">Maximum measurement distance</param>
+        /// <param name="hit">Closest hit distance (if hit)</param>
+        /// <returns>trye if hit, false if not</returns>
+        public bool RayTrace(Vector2 pos, float angle, float maxDist, out float hit)
+        {
+            Vector2 endPos = pos + new Vector2(maxDist * MathF.Cos(angle), maxDist * MathF.Sin(angle));
+            float radius = float.MaxValue;
+
+            // Ray-cast and find closest hit
+            world.RayCast((fixt, hitPos, normal, fract) =>
+            {
+                radius = Math.Min(radius, fract * maxDist);
+            }, pos, endPos);
+
+            // Got any hit ?
+            if (radius < float.MaxValue)
+            {
+                hit = radius;
+                return true;
+            }
+
+            hit = 0.0f;
+            return false;
+        }
+    }
+}
--- Simulation/MainWindow.xaml.cs
+++ Simulation/MainWindow.xaml.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Numerics;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
@@ -13,15 +14,8 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
-using Box2D.NetStandard;
-using Box2D.NetStandard.Collision;
-using Box2D.NetStandard.Collision.Shapes;
-using Box2D.NetStandard.Common;
-using Box2D.NetStandard.Dynamics;
-using Box2D.NetStandard.Dynamics.Bodies;
-using Box2D.NetStandard.Dynamics.Fixtures;
-using Box2D.NetStandard.Dynamics.World;
 using BaseSLAM;
+using CoreSLAM;
 
 namespace Simulation
 {
@@ -30,240 +24,106 @@ namespace Simulation
     /// </summary>
     public partial class MainWindow : Window
     {
-        private World world;
-        private PolygonShape outerShape;
-        private Body outerBody;
-        private Body innerBody;
-        private const int numScanPoints = 100;
-        private const float scanAngle = (MathF.PI * 2) / numScanPoints;
-        private const float maxScanDist = 40.0f; // Meters
+        // Constnts
+        private const int numScanPoints = 600;     // Scan points per revolution        
+        private const float scanPerSecond = 7.0f;  // Scan / second
+        private const float maxScanDist = 40.0f;   // Meters
 
+        // Objects
+        private Field field = new Field();
+        private Vector2 lidarPos = new Vector2(0, 0);
+        private ScaleTransform fieldScale = new ScaleTransform(20, 20);
+        private System.Windows.Threading.DispatcherTimer scanTimer;
+        
+        /// <summary>
+        /// Constructor
+        /// </summary>
         public MainWindow()
         {
             InitializeComponent();
 
-            world = new World(new Vector2(0, 0));
+            // Create field
+            field.CreateDefaultField();
 
-            //Field.RenderTransform = new TranslateTransform(250, 250);
-            Field.RenderTransform = new ScaleTransform(20, 20);
+            // Set render transformation (scaling)
+            Field.RenderTransform = fieldScale;
 
-            CreateField();
-            RenderWorld();
-        }
-
-        private void CreateField()
-        {
-            BodyDef outerBodyDef = new BodyDef
-            {
-                type = BodyType.Static,
-                position = new Vector2(0, 0)
-            };
-
-            outerBody = world.CreateBody(outerBodyDef);
-
-            Vector2[] edges = new Vector2[]
-            {
-                new Vector2(-15.0f, -10.0f),
-                new Vector2(+15.0f, -10.0f),
-                new Vector2(+15.0f,  -7.0f),
-                new Vector2(+12.0f,  -6.0f),
-                new Vector2(+10.0f,  -2.0f),
-                new Vector2(+15.0f,  -2.0f),
-                new Vector2(+15.0f, +10.0f),
-                new Vector2(-7.0f,  +10.0f),
-                new Vector2(-7.0f,  +5.0f),
-                new Vector2(-8.0f,  +5.0f),
-                new Vector2(-8.0f,  +10.0f),
-                new Vector2(-15.0f, +10.0f),
-            };
-
-            //outerShape = new PolygonShape(10, 200);
-            var cs = new ChainShape();
-            cs.CreateLoop(edges);
-
-
-            FixtureDef outerDef = new FixtureDef
-            {
-                shape = cs
-            };
-
-            outerBody.CreateFixture(outerDef);
-
-
-
-            // -----------------------------
-
-            BodyDef innerBodyDef = new BodyDef
-            {
-                type = BodyType.Static,
-                position = new Vector2(0, 0)
-            };
-
-            innerBody = world.CreateBody(innerBodyDef);
-
-            Vector2[] innerEdges = new Vector2[]
-            {
-                new Vector2(-10.0f, -5.0f),
-                new Vector2(-8.0f,  -5.0f),
-                new Vector2(-6.0f,  +1.0f),
-                new Vector2(-8.0f, +2.0f),
-            };
-
-            var innerShape = new PolygonShape(innerEdges);
-
-            //outerShape = new PolygonShape(edges);
-
-
-            FixtureDef innerDef = new FixtureDef
-            {
-                shape = innerShape
-            };
-
-            innerBody.CreateFixture(innerDef);
-        }
-
-        private void RenderWorld()
-        {
-            //RenderPolygon(outerShape.GetVertices(), Brushes.Black);
-
-            Body b = world.GetBodyList();
-
-            while (b != null)
-            {
-
-                //world.GetBodyList().GetNext
-
-                //outerBody.GetFixtureList().GetNext();
-
-                Fixture f = b.GetFixtureList();
-
-
-
-                while (f != null)
-                {
-                    switch (f.Shape)
-                    {
-                        case ChainShape chainShape:
-                            RenderPolygon(chainShape.Vertices, Brushes.Black, 0.1);
-                            break;
-
-                        case PolygonShape polyShape:
-                            RenderPolygon(polyShape.GetVertices(), Brushes.Black, 0.1);
-                            break;
-                    }
-
-                    f = f.GetNext();
-                }
-
-                b = b.GetNext();
-            }
-
-            
+            // Start scan timer
+            scanTimer = new System.Windows.Threading.DispatcherTimer();
+            scanTimer.Tick += (s, e) => Scan();
+            scanTimer.Interval = TimeSpan.FromSeconds(1.0 / scanPerSecond);
+            scanTimer.Start();
         }
 
         /// <summary>
-        /// Render polygon
+        /// Scan
         /// </summary>
-        /// <param name="vertices"></param>
-        /// <param name="brush"></param>
-        private void RenderPolygon(IEnumerable<Vector2> vertices, Brush brush, double thickness)
+        private void Scan()
         {
-            Polygon poly = new Polygon()
+            ScanSegments(lidarPos, out List<ScanSegment> scanSegments);
+            Draw();
+
+            foreach (ScanSegment seg in scanSegments)
             {
-                Fill = Brushes.Silver
+                DrawScan(seg);
+            }
+        }
+
+        /// <summary>
+        /// Draw field and everything
+        /// </summary>
+        private void Draw()
+        {
+            Field.Children.Clear();
+            DrawBackground();
+            DrawField();
+        }
+
+        /// <summary>
+        /// "Draw" background. Need some object on canvas to get mouse events.
+        /// </summary>
+        private void DrawBackground()
+        {
+            // Have 10x10 km rectangle.
+            var bg = new Rectangle()
+            {
+                Fill = Brushes.Silver,
+                Width = 10000.0,
+                Height = 10000.0f
             };
 
-            //Field.Children.Add(poly);
+            Canvas.SetLeft(bg, -5000.0f);
+            Canvas.SetTop(bg, -5000.0f);
 
-            Vector2 prev = vertices.Last();
+            Field.Children.Add(bg);
+        }
 
-            foreach (Vector2 v in vertices)
+        /// <summary>
+        /// "Draw" field edges.
+        /// </summary>
+        private void DrawField()
+        {
+            foreach ((Vector2, Vector2) edge in field.GetEdges())
             {
                 Line line = new Line()
                 {
-                    X1 = prev.X,
-                    Y1 = prev.Y,
-                    X2 = v.X,
-                    Y2 = v.Y,
-                    Stroke = brush,
-                    StrokeThickness = thickness
+                    X1 = edge.Item1.X,
+                    Y1 = edge.Item1.Y,
+                    X2 = edge.Item2.X,
+                    Y2 = edge.Item2.Y,
+                    Stroke = Brushes.Black,
+                    StrokeThickness = 0.1f
                 };
-
-                //poly.Points.Add(new Point(v.X, v.Y));
 
                 Field.Children.Add(line);
-                prev = v;
             }
         }
 
-        private void Scan(Vector2 pos, out List<ScanSegment> segments)
-        {
-            ScanSegment scanSegment = new ScanSegment()
-            {
-                Pose = new Vector3(pos.X, pos.Y, 0),
-                IsLast = true
-            };
-
-            for (float angle = 0.0f; angle < MathF.PI * 2; angle += scanAngle)
-            {
-                Vector2 endPos = pos + new Vector2(maxScanDist * MathF.Cos(angle), maxScanDist * MathF.Sin(angle));
-                List<float> hits = new List<float>();
-
-                world.RayCast((fixt, hitPos, normal, fract) =>
-                {
-                    hits.Add((hitPos - pos).Length());
-                }, pos, endPos);
-
-                if (hits.Count > 0)
-                {
-                    hits.Sort();
-                    scanSegment.Rays.Add(new Ray(angle, hits[0]));
-                }
-            }
-
-            segments = new List<ScanSegment>()
-            {
-                scanSegment
-            };
-        }
-
-        private void Field_MouseMove(object sender, MouseEventArgs e)
-        {
-
-            //if (e.LeftButton == MouseButtonState.Pressed)
-            {
-
-                Point p = e.GetPosition(Field);
-                Vector2 pos = new Vector2((float)p.X, (float)p.Y);
-
-                Scan(pos, out List<ScanSegment> scanSegments);
-
-                Field.Children.Clear();
-                
-                var bg = new Rectangle()
-                {
-                    Fill = Brushes.Silver,
-                    Width = 200.0f,
-                    Height = 200.0f
-                };
-
-                Canvas.SetLeft(bg, -100.0f);
-                Canvas.SetTop(bg, -100.0f);
-
-                Field.Children.Add(bg);
-
-                RenderWorld();
-
-                this.Title = $"{p.X} x {p.Y}";
-
-                foreach (ScanSegment seg in scanSegments)
-                {
-                    RenderScan(seg);
-                }
-            }
-        }
-
-        private void RenderScan(ScanSegment segment)
+        /// <summary>
+        /// Draw scan
+        /// </summary>
+        /// <param name="segment"></param>
+        private void DrawScan(ScanSegment segment)
         {
             foreach (Ray ray in segment.Rays)
             {
@@ -283,6 +143,84 @@ namespace Simulation
 
                 Field.Children.Add(line);
             }
+        }
+
+        /// <summary>
+        /// Scan segments
+        /// </summary>
+        /// <param name="pos"></param>
+        /// <param name="segments"></param>
+        private void ScanSegments(Vector2 pos, out List<ScanSegment> segments)
+        {
+            float scanAngle = (MathF.PI * 2) / numScanPoints;
+
+            ScanSegment scanSegment = new ScanSegment()
+            {
+                Pose = new Vector3(pos.X, pos.Y, 0),
+                IsLast = true
+            };
+
+            for (float angle = 0.0f; angle < MathF.PI * 2; angle += scanAngle)
+            {
+                if (field.RayTrace(pos, angle, maxScanDist, out float hit))
+                {
+                    scanSegment.Rays.Add(new Ray(angle, hit));
+                }
+            }
+
+            segments = new List<ScanSegment>()
+            {
+                scanSegment
+            };
+        }
+
+        /// <summary>
+        /// Mouse move event
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void Field_MouseMove(object sender, MouseEventArgs e)
+        {
+            if (e.LeftButton == MouseButtonState.Pressed)
+            {
+                UpdateLidarPosition(e.GetPosition(Field));
+            }
+        }
+
+        /// <summary>
+        /// Mouse down event
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void Field_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            if (e.LeftButton == MouseButtonState.Pressed)
+            {
+                UpdateLidarPosition(e.GetPosition(Field));
+            }
+        }
+
+        /// <summary>
+        /// Update lidar position
+        /// </summary>
+        /// <param name="p"></param>
+        private void UpdateLidarPosition(Point p)
+        {
+            lidarPos = new Vector2((float)p.X, (float)p.Y);
+        }
+
+        /// <summary>
+        /// Mouse wheel action on canvas.
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void Field_MouseWheel(object sender, MouseWheelEventArgs e)
+        {
+            double delta = Math.Sign(e.Delta) * 1.0;
+            double scale = Math.Max(1.0, Math.Min(100.0, fieldScale.ScaleX + delta));
+
+            fieldScale.ScaleX = scale;
+            fieldScale.ScaleY = scale;
         }
     }
 }
